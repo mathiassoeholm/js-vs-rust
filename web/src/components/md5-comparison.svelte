e148874633dc476bb933cef32b9e87fc08148da9
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-11-12 09:59:36 +0100

    Start working on a Svelte component

---
+++ web/src/components/md5-comparison.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onMount } from "svelte";
+
+  let rustData;
+
+  onMount(() => {
+    const rustWorker = new Worker("rust-worker.js", {
+      name: "my-worker",
+      type: "module",
+    });
+
+    rustWorker.onmessage = function (e) {
+      rustData = e.data;
+    };
+  });
+</script>
+
+<p>hello {rustData}</p>
